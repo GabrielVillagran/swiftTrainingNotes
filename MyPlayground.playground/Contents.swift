2e9f23a1a0cf8ef3b1753d783bac50e22b0a25e6
Gabriel Villagrán <44887537+GabrielVillagran@users.noreply.github.com>
2025-02-19 11:27:45 -0500

    Add more topics

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -15,7 +15,7 @@ Non-Escaping Closures** (Default in Swift):
 
  When choose Escaping or Non-Escaping: One thing we need to consider is if we are using any kind of operation that takes any amount of time is prefer to use Escaping, but if it is something that is not blocking we can go with non-escaping
 
- Race Condition: Happens when the order of events affects the correctnessof a piece of code
+ Race Condition: Happens when the order of events\\\\\\\\\\\\\\\\\\\yo  affects the correctnessof a piece of code
 
  Data Races: A data reace happens when one thread have access to a mutable object while another thread is writing on it, a race condition can happen without a data race and viceversa
 
@@ -111,3 +111,55 @@ class SomeClass {
         }
     }
 }
+///--------------------------------------------------------------------------------------
+/*
+ Closure: A closure is an **anonymous function** that can be passed around and executed later. Closures can capture and store references to variables from their surrounding context, allowing them to retain state even after the original scope has changed.
+
+ Capture List: A capture list allows us to control how values are captured and used inside a closure. It helps prevent memory leaks by specifying whether variables should be captured strongly, weakly, or unowned, especially when working with reference types like classes.
+*/
+
+/// Value Type Closure
+var a = 10
+a = 20
+
+var closure = { [a](val: Int) in   //| The value of the closure is set originally as 10
+    print("val = \(val)")          //| the capture list captures the value at the moment
+    print("a = \(a)")              //| the closure is created, even though a changes to
+}                                  //| 90, the closure still holds the original value
+
+a = 90
+
+closure(100)
+
+/// Reference Type Closure
+class Test {                        //| The values is set originally at 89
+    var unit: Int = 89              //| because this is a reference value,
+}                                   //| we are modifyng the same memory address
+                                    //| therefore, once the closure captures the value
+var obj = Test()                    //| it will capture the latest values that is set
+var closureForClass = { [obj] in    //| in this case, the value has changed to 1
+    print(obj.unit)                 //| and it will be the value that are going to
+}                                   //| be used
+obj.unit = 1                        //|
+closureForClass()                   //|
+///--------------------------------------------------------------------------------------
+/// Optional Chaining
+/*
+Optional chaining: It's a simplified syntax for reading optionals or multiple optional values instead of unwraping them manually. It's rarely used but it's useful to have, basically if the optional have value run some code but, if it's empty, return nil or we can also use coalescing notation for assigning a default value.
+
+Coalescing Notation: It is used for unwrapping optional values, it is useful because instead of having a nil or empty value, if it's nil, we can assign a default value instead of having a nil.
+*/
+
+let names = ["Arya", "Bran", "Edgar","Armando"]
+let chosen = names.randomElement()?.uppercased() ?? "No Name"
+print("Next in line: \(chosen)")
+
+/// Handle Multiple Conditional Values
+struct Book {
+    let title: String
+    let author: String?
+}
+
+var book: Book? = nil
+let author = book?.author?.uppercased() ?? "No Author"
+print(author)
